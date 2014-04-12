ae0b1902c4421ad18cf3585e3bc4404d2fb4ae41
Daniel Merriott <danielmerriott@i-df2051e0.aws>
2014-04-13 11:14:07 +1200

    updated git-deploy conf

--- create-htpasswd.sh
+++
@@ -1,5 +0,0 @@
-# Create htpasswd file
-htpasswd -Bb -c /data/www/openleaf-co-nz/config/htpasswd daniel letmeinnow
-# Append additional users to htpasswd file
-htpasswd -Bb /data/www/openleaf-co-nz/config/htpasswd mary-anne letmeinnow
-htpasswd -Bb /data/www/openleaf-co-nz/config/htpasswd josie cowriter
